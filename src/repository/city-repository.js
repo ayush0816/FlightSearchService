6b0f4f2527c229b1965640908cdeb57479bc7c2c
Ayush Singh <ayush.singh@beepkart.com>
2023-12-10 22:39:55 +0530

    Added routes

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -27,11 +27,14 @@ class CityRepository{
 
     async updateCity(cityId,data){
         try {
-            const city = City.update(data , {
-                where : {
-                    id : cityId
-                }
-            });
+            // const city = City.update(data , {
+            //     where : {
+            //         id : cityId
+            //     }
+            // }); 
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
             return city;
         } catch (error) {
             console.log(error);
