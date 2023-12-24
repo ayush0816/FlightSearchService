5b5e2071a1cc060937179685a67e912f86a98dac
Ayush Singh <ayush.singh@beepkart.com>
2023-12-25 03:47:41 +0530

    Added airplanes model

---
+++ src/seeders/20231213193025-add-airplanes.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Airplanes' , [
+    {
+      modelNumber : 'Boeing 737',
+      capacity : 300,
+      createdat: new Date(), 
+      updatedAt: new Date()
+    },
+    {
+      modelNumber : 'Airbus A320',
+      capacity : 320,
+      createdat: new Date(), 
+      updatedAt: new Date()
+    },
+    {
+      modelNumber : 'Boeing 777',
+      capacity : 360,
+      createdat: new Date(), 
+      updatedAt: new Date()
+    },
+    {
+      modelNumber : 'Boeing 747',
+      capacity : 450,
+      createdat: new Date(), 
+      updatedAt: new Date()
+    },
+    {
+      modelNumber : 'Airbus A320',
+      capacity : 150,
+      createdat: new Date(), 
+      updatedAt: new Date()
+    },
+  ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
